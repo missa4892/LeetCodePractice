e8f5ed4e2e8de9b8ed2641c1a1d70e0cad973485
Saloni Kaur <missa4892@gmail.com>
2020-01-04 02:46:56 +0800

    HasPathWithGivenSum

--- src/HasPathWithGivenSum.java
+++ src/HasPathWithGivenSum.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class HasPathWithGivenSum {
+
+    public boolean hasPath(Node node, int targetSum) {
+
+        boolean ans = false;
+
+        int subSum = targetSum - node.data;
+
+        if (subSum == 0 && node.left == null && node.right == null) {
+           return true;
+        }
+        if (node.left != null) {
+             ans = hasPath(node.left, subSum);
+        }
+        if (node.right != null) {
+            if (!ans) {
+             ans = hasPath(node.right, subSum);
+            }
+        }
+        return ans;
+    }
+
+
+
 }
--- test/HasPathWithGivenSumTest.java
+++ test/HasPathWithGivenSumTest.java
@@ -1,5 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HasPathWithGivenSumTest {
 
+    Node a;
+    HasPathWithGivenSum hasPathWithGivenSum;
+
+    @BeforeEach
+    void setUp() {
+        a = new Node(5);
+
+        Node b = new Node(3);
+
+        Node c = new Node(7);
+
+        Node d = new Node(2);
+
+        Node e = new Node(4);
+
+        Node f = new Node(6);
+
+        Node g = new Node(8);
+
+        a.left = b;
+        a.right = c;
+
+        b.left = d;
+        b.right = e;
+
+        c.left = f;
+        c.right = g;
+
+        hasPathWithGivenSum = new HasPathWithGivenSum();
+    }
+
+    @Test
+    void test1() {
+        assertTrue(hasPathWithGivenSum.hasPath(a, 12));
+    }
+
+    @Test
+    void test2() {
+        assertTrue(hasPathWithGivenSum.hasPath(a, 18));
+    }
+
+    @Test
+    void test3() {
+        assertTrue(hasPathWithGivenSum.hasPath(a, 10));
+    }
 }
