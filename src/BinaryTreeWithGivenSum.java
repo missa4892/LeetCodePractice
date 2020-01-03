d0ddcd879c866d138b27ec461a75fa032998c616
Saloni Kaur <missa4892@gmail.com>
2020-01-04 02:47:36 +0800

    BinaryTreeWithGivenSum

--- src/BinaryTreeWithGivenSum.java
+++ src/BinaryTreeWithGivenSum.java
@@ -1,46 +1,36 @@
 public class BinaryTreeWithGivenSum {
+    private int count = 0;
 
-    public static void main(String[] args){
-        Node a = new Node();
-        a.setValue(5);
+    public int countPaths(Node node, int targetSum) {
 
-        Node b = new Node();
-        b.setValue(3);
-
-        Node c = new Node();
-        c.setValue(7);
-
-        Node d = new Node();
-        d.setValue(2);
-
-        Node e = new Node();
-        e.setValue(4);
-
-        Node f = new Node();
-        f.setValue(6);
-
-        Node g = new Node();
-        g.setValue(8);
-
-        a.setLeft(b);
-        a.setRight(c);
-
-        b.setLeft(d);
-        b.setRight(e);
-
-        c.setLeft(f);
-        c.setRight(g);
-
-        System.out.println(countPathsWithSum(a, 12));
+        int subSum = targetSum - node.data;
+        countPathsFromNode(node.left, subSum);
+        countPathsFromNode(node.right, subSum);
+        return count;
     }
 
-    private static int countPathsWithSum(Node node, int targeSum) {
+    private void countPathsFromNode(Node node, int targetSum) {
+        int subSum = targetSum - node.data;
+
+        if (subSum == 0 && node.left == null && node.right == null) {
+            count++;
+        } else {
+            if (node.left != null) {
+                countPathsFromNode(node.left, subSum);
+            }
+            if (node.right != null) {
+                countPathsFromNode(node.right, subSum);
+            }
+        }
+    }
+
+    public int countPathsWithSum(Node node, int targeSum) {
         if (node == null) return 0;
 
         int pathsFromRoot = countPathsWithSumFromNode(node, targeSum, 0);
 
-        int pathsOnLeft = countPathsWithSum(node.getLeft(), targeSum);
-        int pathsOnRight = countPathsWithSum(node.getRight(), targeSum);
+        int pathsOnLeft = countPathsWithSum(node.left, targeSum);
+        int pathsOnRight = countPathsWithSum(node.right, targeSum);
 
         return pathsFromRoot + pathsOnLeft + pathsOnRight;
 //        return pathsFromRoot;
@@ -49,46 +39,16 @@ public class BinaryTreeWithGivenSum {
     private static int countPathsWithSumFromNode(Node node, int targeSum, int currentSum) {
         if (node == null) return 0;
 
-        currentSum += node.getValue();
+        currentSum += node.data;
 
         int totalPaths = 0;
         if (currentSum == targeSum){
             return totalPaths++;
         }
 
-        totalPaths += countPathsWithSumFromNode(node.getLeft(), targeSum, currentSum);
-        totalPaths += countPathsWithSumFromNode(node.getRight(), targeSum, currentSum);
+        totalPaths += countPathsWithSumFromNode(node.left, targeSum, currentSum);
+        totalPaths += countPathsWithSumFromNode(node.right, targeSum, currentSum);
 
         return totalPaths;
-    }
-
-    private static class Node {
-        private int value;
-        private Node right;
-        private Node left;
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
     }
 }
--- test/BinaryTreeWithGivenSumTest.java
+++ test/BinaryTreeWithGivenSumTest.java
@@ -1,5 +1,73 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeWithGivenSumTest {
 
+    Node a;
+    BinaryTreeWithGivenSum binaryTreeWithGivenSum = new BinaryTreeWithGivenSum();
+
+    @BeforeEach
+    void setUp() {
+        a = new Node(5);
+
+        Node b = new Node(3);
+
+        Node c = new Node(7);
+
+        Node d = new Node(2);
+
+        Node e = new Node(4);
+
+        Node f = new Node(6);
+
+        Node g = new Node(8);
+
+        a.left = b;
+        a.right = c;
+
+        b.left = d;
+        b.right = e;
+
+        c.left = f;
+        c.right = g;
+
+    }
+
+    @Test
+    void test1() {
+        assertEquals(1, binaryTreeWithGivenSum.countPaths(a, 12));
+    }
+
+    @Test
+    void test2() {
+        a = new Node(10);
+
+        Node b = new Node(5);
+        Node c = new Node(-3);
+        Node d = new Node(3);
+        Node e = new Node(2);
+        Node f = new Node(11);
+        Node g = new Node(3);
+        Node h = new Node(-2);
+        Node i = new Node(1);
+
+        a.left = b;
+        a.right = c;
+
+        b.left = d;
+        b.right = e;
+
+        c.left = f;
+        c.right = null;
+
+        d.left = g;
+        d.right = h;
+
+        e.right = i;
+
+        assertEquals(3, binaryTreeWithGivenSum.countPaths(a, 8));
+    }
 }
+
