cb1acc4db91b904987ef4556e4e14c5c5376ed80
Saloni Kaur <missa4892@gmail.com>
2020-01-04 02:46:07 +0800

    TakeoutTakein

--- src/TakeOutEatIn.java
+++ src/TakeOutEatIn.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class TakeOutEatIn {
+
+    /*
+        Take Out Orders: [1, 3, 5]
+                             |
+         Dine In Orders: [2, 4, 6]
+                             |
+          Served Orders: [1, 2, 4, 6, 5, 3]
+                                |
+     */
+
+    public boolean isFirstComeFirstServed(int[] takeOut, int[] dineIn, int[] servedOrder) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        boolean isAllGood = false;
+        while(i < takeOut.length && j < dineIn.length && k < servedOrder.length) {
+            if(takeOut[i] < dineIn[j]) {
+                if(servedOrder[k] == takeOut[i]) {
+                    isAllGood = true;
+                    i++;
+                    k++;
+                } else {
+                    return false;
+                }
+            } else if(dineIn[j] < takeOut[i]) {
+                if(servedOrder[k] == dineIn[j]) {
+                    isAllGood = true;
+                    j++;
+                    k++;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- test/TakeOutEatInTest.java
+++ test/TakeOutEatInTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TakeOutEatInTest {
 
+    @Test
+    void test1() {
+        TakeOutEatIn takeOutEatIn = new TakeOutEatIn();
+        int[] takeOut = {1, 3, 5};
+        int[] dineIn = {2, 4, 6};
+        int[] servedOrders = {1, 2, 4, 6, 5, 3};
+
+        boolean result = takeOutEatIn.isFirstComeFirstServed(takeOut, dineIn, servedOrders);
+
+        assertFalse(result);
+    }
 }
