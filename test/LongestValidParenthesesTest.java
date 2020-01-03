ff5618fa54ce3933335ebca6e1df089c4ae932bc
Saloni Kaur <missa4892@gmail.com>
2020-01-04 02:34:01 +0800

    LongestValidParantheses

---
+++ test/LongestValidParenthesesTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestValidParenthesesTest {
+
+    @Test
+    void test1() {
+
+        int result = LongestValidParentheses.longestValidParentheses("()(()");
+        assertEquals(2, result);
+    }
+
+    @Test
+    void test2() {
+
+        int result = LongestValidParentheses.longestValidParentheses("()(())");
+        assertEquals(6, result);
+    }
+
+}
